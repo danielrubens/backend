eacfa3899da50ca717a71d3bf6f5331925dea744
Márcio Daniel <marcio.daniel@msn.com>
2022-09-17 20:45:50 -0300

    Middleware test ok

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,17 +1,18 @@
 const rescue = require('express-rescue');
+const { loginService } = require('../services');
 
 const signIn = async (req, res) => { 
   const { email, password } = req.body;
   console.log(email, password);
-  // const token = await loginService.signIn(email, password);
-  res.status(200).json({ token: '123456789' });
+  const token = await loginService.signIn(email, password);
+  res.status(200).json({ token });
 };
 
 const sigUp = async (req, res) => {
   const { name, email, password } = req.body;
   console.log(name, email, password);
-  /// / const token = await loginService.signUp(name, email, password);
-  res.status(200).json({ token: '123456789' });
+  const token = await loginService.signUp(name, email, password);
+  res.status(200).json({ token });
 };
 
 module.exports = {
