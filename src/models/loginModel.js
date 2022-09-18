08de117eed9421c0a087fcaf37530c72c0e84527
Márcio Daniel <marcio.daniel@msn.com>
2022-09-17 23:19:39 -0300

    Model login created

--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -3,13 +3,20 @@ const getConnection = require('./connection');
 const sigIn = async (email) => {
   const query = 'SELECT * FROM weather.user WHERE email = ?';
   const connection = await getConnection();
-  const result = await connection.execute(query, [email]);
-  console.log(result);
+  const[[result]] = await connection.execute(query, [email]);
   return result;
 }
 
-sigIn('marcio.daniel@msn.com');
+const sigUp = async (id, name, email, password) => {
+  const query = 'INSERT INTO weather.user (id, name, email, password) VALUES (?, ?, ?, ?)';
+  const connection = await getConnection();
+  const [result] = await connection.execute(query, [id, name, email, password]);
+  return {
+    id: result.insertId,
+  }
+}
 
 module.exports = {
   sigIn,
+  sigUp,
 };
