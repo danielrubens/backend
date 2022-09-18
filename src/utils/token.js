4e8df93c62c2a18f3e743ca2424a39432cd3d578
Márcio Daniel <marcio.daniel@msn.com>
2022-09-17 23:25:15 -0300

    Create generate and validate Token

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,1 +1,31 @@
-import jwt from 'jsonwebtoken';
+const jwt = require('jsonwebtoken');
+
+const generateToken = ({id, name}) => {
+  const options = {
+    expiresIn: '14d',
+    algorithm: 'HS256',
+  }
+  const token = jwt.sign({id, name}, process.env.JWT_SECRET, options);
+
+  return token;
+};
+
+const validateToken = (token) => {
+  try{
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return {
+      payload: decoded,
+      validated: true,
+    }
+  } catch (error) {
+    return {
+      payload: null,
+      validated: false,
+    }
+  }
+}
+
+module.exports = {
+  generateToken,
+  validateToken,
+};
