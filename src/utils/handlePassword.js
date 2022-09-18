282d6f4dc6e604e10eb83eb97762239b68b5e045
Márcio Daniel <marcio.daniel@msn.com>
2022-09-17 23:39:07 -0300

    Created handle password

---
+++ src/utils/handlePassword.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcrypt');
+
+const comparePassword = async (password, hash) => {
+  const isValid = await bcrypt.compare(password, hash);
+  return isValid;
+}
+
+const createHashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+}
+
+module.exports = {
+  comparePassword,
+  createHashPassword,
+} 
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,11 @@
 const createId = require('./createId');
-
 const token = require('./token');
+const comparePassword = require('./handlePassword');
+const handlePassword = require('./handlePassword');
 
 module.exports = {
   createId,
   token,
+  comparePassword,
+  handlePassword,
 };
